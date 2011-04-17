9b7e2060f6cd74d8811283f8853a528fc3404e96
robert-schmidtke@gmx.de <robert-schmidtke@gmx.de@d2596a6a-bc9f-9154-c08c-ff40c9aceadf>
2011-04-17 18:23:11 +0000

    facebook panel -- built in birthdays and events, still a bug when logged in and reloading

--- trunk/WhatDoINeedToday/src/java/de/hwr/wdint/facebook/FacebookPanel.java
+++ trunk/WhatDoINeedToday/src/java/de/hwr/wdint/facebook/FacebookPanel.java
@@ -5,16 +5,27 @@
 
 package de.hwr.wdint.facebook;
 
+import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.Event;
 import com.restfb.types.User;
+import com.restfb.types.Venue;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import org.apache.wicket.RequestCycle;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
 /**
  *
@@ -38,14 +49,14 @@ public final class FacebookPanel extends Panel {
 	/**needs to be a container because the login button is a child element*/
 	private WebMarkupContainer fbLoginButton;
 
-	/**container for all information div*/
+	/**container for the data view*/
 	private WebMarkupContainer fbInformation;
+	
+	/**displays the data*/
+	private DataView fbDataView;
 
-	/**
-	 * label for welcome text and its text
-	 */
-	private Label labelWelcome;
-	private String labelWelcomeText;
+	/**list contains all our data*/
+	private ArrayList<String> fbDataList;
 
 	/**
 	 * constructor
@@ -55,18 +66,34 @@ public final class FacebookPanel extends Panel {
 
         super (id);
 
+		//title of the panel
+		add(new Label("labelTitle", "Dein Facebook"));
+
 		//add the login button
 		fbLoginButton = new WebMarkupContainer("fbLoginButton");
 		add(fbLoginButton);
 
 		//the fb information container
 		fbInformation = new WebMarkupContainer("fbInformation");
+		fbInformation.setOutputMarkupId(true);
 		add(fbInformation);
 
-		labelWelcomeText = "Hallo ...";
-		labelWelcome = new Label("welcomeLabel", new PropertyModel(this, "labelWelcomeText"));
-		labelWelcome.setOutputMarkupId(true);
-		fbInformation.add(labelWelcome);
+		//the actual information
+		fbDataList = new ArrayList<String>();
+
+		//this will populate the list
+		fbDataView = new DataView("fbEntry", new ListDataProvider(fbDataList)) {
+
+			@Override
+			protected void populateItem(Item item) {
+
+				item.add(new Label("fbEntryContent", (String) item.getModelObject()));
+
+			}
+			
+		};
+
+		fbInformation.add(fbDataView);
 		
     }
 
@@ -117,23 +144,131 @@ public final class FacebookPanel extends Panel {
 
 		//get basic user info
 		User user = fbClient.fetchObject("me", User.class);
+		fbDataList.add("Hallo " + user.getFirstName() + "!");
 
-		//create a "welcome" line
-		labelWelcomeText = "Hallo " + user.getFirstName() + "!";
-		target.addComponent(labelWelcome);
+		//process all birthdays
+		Connection<User> friends = fbClient.fetchConnection("me/friends", User.class, Parameter.with("fields", "birthday"));
+		processBirthdays(friends);
+		while(friends.hasNext()) {
 
-		//TODO get events, birthdays, ...
+			friends = fbClient.fetchConnectionPage(friends.getNextPageUrl(), User.class);
+			processBirthdays(friends);
+
+		}
+
+		//process all events
+		Connection<Event> events = fbClient.fetchConnection("me/events", Event.class);
+		processEvents(events);
+		while(events.hasNext()) {
+
+			events = fbClient.fetchConnectionPage(events.getNextPageUrl(), Event.class);
+			processEvents(events);
+
+		}
+
+		target.addComponent(fbInformation);
 
 	}
 
 	/**
-	 * resets all the info labels
+	 * adds birthdays to the list
+	 * @param friends
+	 */
+	private void processBirthdays(Connection<User> friends) {
+
+		List<User> friendList = friends.getData();
+		for(User friend : friendList) {
+
+			Date birthday = friend.getBirthdayAsDate();
+			if(birthday != null && isToday(birthday)) {
+
+				fbDataList.add(friend.getFirstName() + " hat heute Geburtstag - besorge ein Geschenk!");
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * adds events to the list
+	 * @param events
+	 */
+	private void processEvents(Connection<Event> events) {
+
+		List<Event> eventList = events.getData();
+		for(Event event : eventList) {
+
+			//list includes past and future events
+			Date eventDate = event.getStartTime();
+			if(isToday(event.getStartTime())) {
+
+				//check location
+				String eventLine = event.getName();
+				String eventLocation = event.getLocation();
+				if(eventLocation == null) {
+
+					//maybe an address?
+					Venue venue = event.getVenue();
+					if(venue == null) {
+
+						//no location, no venue, maybe description
+						eventLine += " (" + (event.getDescription() != null ? event.getDescription() : "weder Ort noch Beschreibung vorhanden") + ")";
+
+					} else {
+
+						//no location but a venue
+						eventLocation = venue.getCity() != null ? (venue.getCity() + " ") : "";
+						eventLocation += venue.getStreet() != null ? venue.getStreet() : "";
+
+					}
+
+				} else {
+
+					//location is set
+					eventLine += " in " + eventLocation;
+
+				}
+
+				//we only need the time because it is today
+				eventLine += " um " + new SimpleDateFormat("HH:mm").format(eventDate) + " Uhr";
+
+				fbDataList.add(eventLine);
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * resets all content
 	 * @param target
 	 */
 	private void resetFBInformation(AjaxRequestTarget target) {
 
-		labelWelcomeText = "Hallo ...";
-		target.addComponent(labelWelcome);
+		fbDataList.clear();
+		target.addComponent(fbInformation);
+
+	}
+
+	/**
+	 * obviously i was lazy ...
+	 * http://www.computing.net/answers/programming/comparing-a-date-object-with-today/17209.html
+	 * @param date
+	 * @return
+	 */
+	private static boolean isToday(Date date) {
+
+		Calendar today = Calendar.getInstance();
+		today.setTime(new Date());
+
+		Calendar otherday = Calendar.getInstance();
+		otherday.setTime(date);
+
+		return	otherday.get(Calendar.YEAR) == today.get(Calendar.YEAR) &&
+				otherday.get(Calendar.MONTH) == today.get(Calendar.MONTH) &&
+				otherday.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH);
 
 	}
 
