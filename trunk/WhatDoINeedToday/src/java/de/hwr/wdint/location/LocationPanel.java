983d458c4a20fa4267843647e285214cd3daf216
Julius.Ollesch <Julius.Ollesch@d2596a6a-bc9f-9154-c08c-ff40c9aceadf>
2011-04-19 07:41:39 +0000

    Location Panel wieder funktionstuechtig und enhanced

--- trunk/WhatDoINeedToday/src/java/de/hwr/wdint/location/LocationPanel.java
+++ trunk/WhatDoINeedToday/src/java/de/hwr/wdint/location/LocationPanel.java
@@ -2,14 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.hwr.wdint.location;
+
 import de.hwr.wdint.BasePage;
 import de.hwr.wdint.HomePage;
-import java.util.Locale;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
-import org.apache.wicket.extensions.ajax.markup.html.AjaxIndicatorAppender;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.PropertyModel;
 
@@ -20,48 +19,51 @@ import org.apache.wicket.model.PropertyModel;
 public final class LocationPanel extends Panel {
 
     //Variable für das Datenmodell des Labels des Pannels
-    private String labelTitleText = "";
+    private String labelUserInputCityText = "";
 
-    public String getLabelText() {
-        return labelTitleText;
+    public String getLabelUserInputCityText() {
+        return labelUserInputCityText;
     }
 
-    public void setLabelText(String labelText) {
-        this.labelTitleText = labelText;
+    public void setLabelUserInputCityText(String labelText) {
+        this.labelUserInputCityText = labelText;
     }
-
-   
-
     //Label des Pannels ist ein AjaxEditableLabel, so dass Benutzer ihren Aufenthaltsort überschreiben können
-    AjaxEditableLabel title = new AjaxEditableLabel("title", new PropertyModel(this, "labelTitleText")) {
+    AjaxEditableLabel userInputCity = new AjaxEditableLabel("userInputCity", new PropertyModel(this, "labelUserInputCityText")) {
         //Methode onSubmit wird überschrieben
+
         @Override
         protected void onSubmit(AjaxRequestTarget target) {
             if (target != null) {
                 super.onSubmit(target);
-                if (labelTitleText != null) {
-                    if (!labelTitleText.equalsIgnoreCase("")) {
-                        
-                        
-                        
-                        System.out.println(labelTitleText);
+                if (labelUserInputCityText != null) {
+                    if (!labelUserInputCityText.equalsIgnoreCase("")) {
+
+
+                        System.out.println(labelUserInputCityText);
 
                         //Falls etwas eigegeben wurde, setze userLocation mit der Eingabe
-                        ((BasePage)this.getPage()).setUserLocation(labelTitleText);
-                        ((BasePage)this.getPage()).update(target);
+                        ((BasePage) this.getPage()).setUserLocation(labelUserInputCityText);
+                        ((BasePage) this.getPage()).update(target);
                     }
 
                     //zeige die Region des Benutzers im Label an
-                    labelTitleText = ((BasePage)this.getPage()).getUserLocation().getUrbanArea();
-                    //aktualisiere das Label in der HTML
-                    target.addComponent(title);
-                    //Rufe Methode zum aktualisieren der Tabelle mit Events auf
-                    //Der Benutzer bekommt so nach Eingabe eine aktualisierte Ansicht
-                    //updateDataViewWithEvents(target);
+                    labelUserInputCityText = ((BasePage) this.getPage()).getUserLocation().getCity();
+                    labelUserUrbanAreaText = ((HomePage) this.getPage()).getUserLocation().getUrbanArea();
+
+                    //aktualisiere die Label in der HTML
+                    target.addComponent(userInputCity);
+                    target.addComponent(userUrbanArea);
+
 
                 } else {
                     //Falls Text Null war setze den text des Labels wieder zurück auf die Region des Benutzers
-                    labelTitleText = ((BasePage)this.getPage()).getUserLocation().getUrbanArea();
+                    labelUserInputCityText = ((BasePage) this.getPage()).getUserLocation().getCity();
+                    labelUserUrbanAreaText = ((HomePage) this.getPage()).getUserLocation().getUrbanArea();
+                    
+                    //aktualisiere die Label in der HTML
+                    target.addComponent(userInputCity);
+                    target.addComponent(userUrbanArea);
                 }
             }
         }
@@ -71,27 +73,35 @@ public final class LocationPanel extends Panel {
             setOutputMarkupId(true);
 
             setEscapeModelStrings(true);
-            
+
         }
     };
+    private String labelUserUrbanAreaText = "";
 
+    public String getLabelUserUrbanArea() {
+        return labelUserUrbanAreaText;
+    }
 
+    public void setLabelUserUrbanArea(String labelUserUrbanArea) {
+        this.labelUserUrbanAreaText = labelUserUrbanArea;
+    }
+    Label userUrbanArea = new Label("userUrbanArea", new PropertyModel(this, "labelUserUrbanAreaText"));
 
     public LocationPanel(String id) {
-        super (id);
+        super(id);
         //Wichtig damit ajax funktioniert! So wird die ID des Elements in die HTML übergeben
         this.setOutputMarkupId(true);
         //Setze den Text des Labels mit der Region
-  
+
 
     }
 
     @Override
     protected void onBeforeRender() {
         super.onBeforeRender();
-        labelTitleText = ((HomePage)this.getPage()).getUserLocation().getUrbanArea();
-        add(title);
+        labelUserInputCityText = ((HomePage) this.getPage()).getUserLocation().getCity();
+        labelUserUrbanAreaText = ((HomePage) this.getPage()).getUserLocation().getUrbanArea();
+        add(userInputCity);
+        add(userUrbanArea);
     }
-
-    
 }
