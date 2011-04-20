64d366fe32278a3c1e73ecf076f1fa70e1e4a849
Julius.Ollesch <Julius.Ollesch@d2596a6a-bc9f-9154-c08c-ff40c9aceadf>
2011-04-20 10:37:50 +0000

    Autocolpete verbessert - benutzt jetzt geonames.org

--- WhatDoINeedToday/src/java/de/hwr/wdint/location/LocationAutoCompleteBehavior.java
+++ WhatDoINeedToday/src/java/de/hwr/wdint/location/LocationAutoCompleteBehavior.java
@@ -4,13 +4,19 @@
  */
 package de.hwr.wdint.location;
 
-
 import de.hwr.wdint.location.Regions;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteBehavior;
 import org.apache.wicket.extensions.ajax.markup.html.autocomplete.StringAutoCompleteRenderer;
+import org.geonames.Style;
+import org.geonames.Toponym;
+import org.geonames.ToponymSearchCriteria;
+import org.geonames.ToponymSearchResult;
+import org.geonames.WebService;
 
 /**
  *
@@ -20,21 +26,48 @@ public class LocationAutoCompleteBehavior extends AutoCompleteBehavior {
 
     @Override
     protected Iterator getChoices(String input) {
-        List subList = new ArrayList();
-        for (Regions r : Regions.values()){
-            if(r.name().toLowerCase().startsWith(input.toLowerCase())){
-                subList.add(r);
+        try {
+            List subList = new ArrayList();
+
+            WebService.setUserName("wdint");
+
+            //Bekomme eine Liste Städte
+            ToponymSearchCriteria searchCriteria = new ToponymSearchCriteria();
+            //Sprache ist Deutsch - wie ganz WDINT
+            searchCriteria.setLanguage("de");
+
+            //alles was mit der Eingabe des Benutzers anfängt
+            searchCriteria.setNameStartsWith(input);
+
+            //Hole maximal 10 Einträge , macht weniger Traffic und ist übersichtlicher
+            searchCriteria.setMaxRows(10);
+            //und in Deutschland ist
+            searchCriteria.setCountryCode("DE");
+            //und eine Stadt ist
+            searchCriteria.setFeatureCode("PPL");
+
+            ToponymSearchResult searchResult = WebService.search(searchCriteria);
+
+
+            for (Toponym toponym : searchResult.getToponyms()) {
+                String cityName = toponym.getName();
+                //Zeige dem Benutzer maximal 5 Einträge an
+                if(subList.size() == 5){
+                    break;
+                }
+                //Manchmal werden doppelte Einträge geliefert - die müssen manuell aussortiert werden
+                if (!subList.contains(cityName)) {
+                    subList.add(cityName);
+                }
             }
+            return subList.iterator();
+        } catch (Exception ex) {
+            Logger.getLogger(LocationAutoCompleteBehavior.class.getName()).log(Level.SEVERE, null, ex);
+            return new ArrayList().iterator();
         }
-        return subList.iterator();
     }
 
-   
-
-    public LocationAutoCompleteBehavior(){
-       super(new StringAutoCompleteRenderer());
+    public LocationAutoCompleteBehavior() {
+        super(new StringAutoCompleteRenderer());
     }
-
-
-
 }
